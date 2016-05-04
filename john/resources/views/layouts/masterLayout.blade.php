a74f7be5ba3792795d82db22963ddbf4f477fc85
jb <decastro.johnbendict@gmail.com>
2016-05-04 10:59:40 +0800

    ped

--- john/resources/views/layouts/masterLayout.blade.php
+++ john/resources/views/layouts/masterLayout.blade.php
@@ -245,11 +245,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- /.search form -->
 
           <!-- Sidebar Menu -->
-          <ul class="sidebar-menu">
-            <li class="header">HEADER</li>
+          <ul class="sidebar-menu" >
+            <li class="header">Menu</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="dashboard"><i class="fa fa-link"></i> <span>Dashboard</span></a></li>
-            <li><a href="patientRecords"><i class="fa fa-link"></i> <span>Patient Records</span></a></li>
+            <li ><a href="patientRecords"><i class="fa fa-link"></i> <span>Patient Records</span></a></li>
             <li class="treeview">
               <a href=""><i class="fa fa-link"></i> <span>Management</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
--- john/resources/views/pages/homePage.blade.php
+++ john/resources/views/pages/homePage.blade.php
@@ -4,67 +4,266 @@
 
 @section('content')
 	<section class="content-header">
-          <h1>
-            Dashboard
-            <small>Patient Files</small>
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
-          </ol>
-        </section>
-	 
+	          <h1>
+	            Dashboard
+	            <small>Patient Files</small>
+	          </h1>
+	          <ol class="breadcrumb">
+	            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+	            <li class="active">Here</li>
+	          </ol>
+	</section>
+		 <hr>
+
+
+<div class="panel panel-default">
+  <div class="panel-body">
   
         <div class="container-fluid">
         	<div class="row">  <! Start // ROW  -->
-        		<div class="col-lg-6 col-xs-12">	 <! FIRST TABLE  -->
-        			<div> 	<!-- TITLE **** --> 
-						<h3> Recent Consultation </h3>
-					</div>	<! end title 1 //>
+        		<div class="col-lg-6 col-xs-12 table-responsive " >	 
+        		<! FIRST TABLE  -->
+        			
 
 					<!-- TABLE RECENT CONSULTATION /  -->
-						<table class="table table-bordered table-hover table-responsive table-striped table-fixed ">
-								<thead id="patient_table">
-										<th> Patient ID </th>
-										<th> Name</th>
-									
-										<th> Menu </th>
-								</thead>		
-			 				<tbody>
-	 							<tr>
-	 								<td #patient>13010231 </td>
-	 								<td #patient> Steeve </td>
-	 								<td>
-										<a type="button" class="btn btn-primary" href="viewProfile" >View Profile</a> 
-	 								</td>
-	 							</tr>				
-	 						</tbody>
-						</table>		
+					<div class="box table-hover" style="background-color: skyblue">
+                <div class="box-header">
+                  <h3 class="box-title">Recent Consultations</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                    <thead>
+                      <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 177px;">Rendering engine</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 224px;">Browser</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 205px;">Platform(s)</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 152px;">Engine version</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 110px;">CSS grade</th></tr>
+                    </thead>
+                    <tbody>
+                    <tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 1.0</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 1.5</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 2.0</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 3.0</td>
+                        <td>Win 2k+ / OSX.3+</td>
+                        <td>1.9</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Camino 1.0</td>
+                        <td>OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Camino 1.5</td>
+                        <td>OSX.3+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape 7.2</td>
+                        <td>Win 95+ / Mac OS 8.6-9.2</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape Browser 8</td>
+                        <td>Win 98SE+</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape Navigator 9</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Mozilla 1.0</td>
+                        <td>Win 95+ / OSX.1+</td>
+                        <td>1</td>
+                        <td>A</td>
+                      </tr></tbody>
+                    <tfoot>
+                      <tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th></tr>
+                    </tfoot>
+                  </table>
+                  </div>
+                  </div>
+                  <div class="row">
+                  	<div class="col-sm-5">
+                  		<div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries
+                  	</div>
+                  </div>
+                  <div class="col-sm-7">
+                  	<div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                  		<ul class="pagination">
+                  			<li class="paginate_button previous disabled" id="example1_previous">
+                  				<a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a>
+                  			</li>
+                  			<li class="paginate_button active">
+                  				<a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a>
+                  			</li>
+                  			<li class="paginate_button next" id="example1_next">
+                  				<a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a>
+                  			</li>
+                  		</ul>
+                  	</div>
+                  </div>
+                  </div>
+                  </div>
+                </div><!-- /.box-body -->
+              </div>	
 				</div>
 
-				<div class="col-lg-6 col-xs-12">	 <! SECOND TABLE  -->
-        			<div> 	<!-- TITLE 1 **** --> 
-						<h3> Alerts / Reminders </h3>
-					</div>	<! end title 2 //>
+				<div class="col-lg-6 col-xs-12 table-responsive">	 <! SECOND TABLE  -->
+        			
 
 					<!-- TABLE RECENT CONSULTATION /  -->
-						<table class="table table-bordered table-hover table-responsive table-striped table-fixed">
-								<thead id="patient_table">
-										<th> Patient ID </th>
-										<th> Name</th>
-									
-										<th> Menu </th>
-								</thead>		
-			 				<tbody>
-	 							<tr>
-	 								<td #patient>13010231 </td>
-	 								<td #patient> Steeve </td>
-	 								<td>
-										<a type="button" class="btn btn-primary" href="viewProfile" >View Profile</a> 
-	 								</td>
-	 							</tr>				
-	 						</tbody>
-						</table>		
+						<div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Alert/Reminders</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                    <thead>
+                      <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 177px;">Rendering engine</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 224px;">Browser</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 205px;">Platform(s)</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 152px;">Engine version</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 110px;">CSS grade</th></tr>
+                    </thead>
+                    <tbody>
+                    <tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 1.0</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 1.5</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 2.0</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Firefox 3.0</td>
+                        <td>Win 2k+ / OSX.3+</td>
+                        <td>1.9</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Camino 1.0</td>
+                        <td>OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Camino 1.5</td>
+                        <td>OSX.3+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape 7.2</td>
+                        <td>Win 95+ / Mac OS 8.6-9.2</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape Browser 8</td>
+                        <td>Win 98SE+</td>
+                        <td>1.7</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="odd">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Netscape Navigator 9</td>
+                        <td>Win 98+ / OSX.2+</td>
+                        <td>1.8</td>
+                        <td>A</td>
+                      </tr><tr role="row" class="even">
+                        <td class="sorting_1">Gecko</td>
+                        <td>Mozilla 1.0</td>
+                        <td>Win 95+ / OSX.1+</td>
+                        <td>1</td>
+                        <td>A</td>
+                      </tr></tbody>
+                    <tfoot>
+                      <tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th></tr>
+                    </tfoot>
+                  </table>
+                  </div>
+                  </div>
+                  <div class="row">
+                  	<div class="col-sm-5">
+                  		<div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries
+                  	</div>
+                  </div>
+                  <div class="col-sm-7">
+                  	<div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                  		<ul class="pagination">
+                  			<li class="paginate_button previous disabled" id="example1_previous">
+                  				<a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0">Previous</a>
+                  			</li>
+                  			<li class="paginate_button active">
+                  				<a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0">1</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0">2</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0">3</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0">4</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0">5</a>
+                  			</li>
+                  			<li class="paginate_button ">
+                  				<a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0">6</a>
+                  			</li>
+                  			<li class="paginate_button next" id="example1_next">
+                  				<a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0">Next</a>
+                  			</li>
+                  		</ul>
+                  	</div>
+                  </div>
+                  </div>
+                  </div>
+                </div><!-- /.box-body -->
+              </div>		
 				</div>
 
 						
@@ -73,6 +272,8 @@
         </div>
 
      
+</div>
+</div>
 			
 	
             

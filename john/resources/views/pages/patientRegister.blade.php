f537a08afc02c96eaea718b86326a810d95fb522
jb <decastro.johnbendict@gmail.com>
2016-05-24 18:02:26 +0800

    AddPatient : Added New Fields

--- john/resources/views/pages/patientRegister.blade.php
+++ john/resources/views/pages/patientRegister.blade.php
@@ -59,9 +59,35 @@
 		</select>
 	
  	</div>
+
+ 	<div class="col-lg-6">
+ 		<select class="c-select" style="height: 30px; width:75%">
+		  <option selected>Gender</option>
+		  <option value="1">One</option>
+		  <option value="2">Two</option>
+		  <option value="3">Three</option>
+		</select>
+	
+ 	</div>
  	
  	</div>
 
+ 	<div class="row form-group ">
+	<div class="col-lg-6">
+ 		<select class="c-select" style="height: 30px; width:75%">
+		  <option selected>Government Type ID</option>
+		  <option value="1">One</option>
+		  <option value="2">Two</option>
+		  <option value="3">Three</option>
+		</select>
+	
+ 	</div>
+ 	
+	<div class="col-lg-6">
+		<input type="text" class="form-control" placeholder="Government ID Number" aria-describedby="basic-addon1">
+	</div>
+	
+</div>
 
 <div >
 	<hr>
@@ -73,23 +99,91 @@
 </div>
 <div class="row form-group ">
 	
-	<div class="col-lg-6">
-		<input type="text" class="form-control" placeholder="Home Address" aria-describedby="basic-addon1" required>
-	</div>
-	<div class="col-lg-6">
+	<div class="col-lg-4">
 		<input type="text" class="form-control" placeholder="Email Adress" aria-describedby="basic-addon1">
 	</div>
-</div>
 
-<div class="row form-group ">
-	<div class="col-lg-6">
+	<div class="col-lg-4">
 		<input type="text" class="form-control" placeholder="Mobile Number" aria-describedby="basic-addon1" required>
 	</div>
-	<div class="col-lg-6">
+	<div class="col-lg-4">
 		<input type="text" class="form-control" placeholder="Phone Number" aria-describedby="basic-addon1">
 	</div>
 	
 </div>
+	
+	<hr>
+
+
+</div>
+
+	<div class="container">
+		<h4 style="font-weight: bold;">Emergency Contact</h4>
+	</div>
+
+<div class="row form-group container-fluid">
+	
+	<div class="col-lg-4">
+		<input type="text" class="form-control" placeholder="Name" aria-describedby="basic-addon1">
+	</div>
+
+	<div class="col-lg-4">
+		<input type="text" class="form-control" placeholder="Middle Name" aria-describedby="basic-addon1" required>
+	</div>
+
+	<div class="col-lg-4">
+		<input type="text" class="form-control" placeholder="Surname" aria-describedby="basic-addon1">
+	</div>
+	
+</div>
+
+<div class="row form-group container-fluid">
+	
+	<div class="col-lg-6">
+		<input type="text" class="form-control" placeholder="Emrgency Contact Number" aria-describedby="basic-addon1">
+	</div>
+
+	<div class="col-lg-6">
+		<input type="text" class="form-control" placeholder="Relationship" aria-describedby="basic-addon1" required>
+	</div>
+	
+</div>
+
+<hr>
+
+<div class="container">
+	<h4 style="font-weight: bold;">Address</h4>
+</div>
+	
+
+<div class="row form-group container">		
+		<div class="col-lg-8">
+			<input type="text" class="form-control" placeholder="Street" aria-describedby="basic-addon1">
+		</div>	
+</div>
+
+<div class="row form-group container">
+		
+		<div class="col-lg-4">
+			<input type="text" class="form-control" placeholder="Barangay/District" aria-describedby="basic-addon1" required>
+		</div>
+		<div class="col-lg-4">
+			<input type="text" class="form-control" placeholder="City" aria-describedby="basic-addon1">
+		</div>
+</div>
+
+<div class="row form-group container">
+		
+		<div class="col-lg-4">
+			<input type="text" class="form-control" placeholder="Province" aria-describedby="basic-addon1" required>
+		</div>
+		<div class="col-lg-4">
+			<input type="text" class="form-control" placeholder="Zip Code" aria-describedby="basic-addon1">
+		</div>
+
+		
+</div>
+
 
 <hr>
 

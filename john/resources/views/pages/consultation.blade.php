05943e1e04d4e7ce69ba43208ed3efdaa3002f7f
Marvin Baste <marvinbaste000@gmail.com>
2016-06-01 14:47:33 -0700

    Added : Modal

---
+++ john/resources/views/pages/consultation.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.masterLayout')
+
+
+
+
+@section('title', 'Consultation | Patient Files')
+@section('content')
+
+<section class="content-header">
+          <h1>
+           Consultation
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#">consultation</a></li>
+          </ol>
+      </section>
+      <hr>
+
+
+
+      <div  id="consultation"></div>
+
+@stop

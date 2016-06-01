1b13516aff2aefa952ec1c2ad955b1e060f90bb9
jb <decastro.johnbendict@gmail.com>
2016-06-01 09:39:06 +0800

    Login : fix sign_in

--- john/resources/views/layouts/headerLayout.blade.php
+++ john/resources/views/layouts/headerLayout.blade.php
@@ -15,10 +15,15 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right" >
-                    <li >
+                
+                <!-- NAV FOR RIGHT  -->
+
+                <!--    <li >
                         <a style="color: white" class="page-scroll " href="#about">Sign In</a>
                     </li>
-                   
+                -->
+
+
                 </ul>
             </div>
             <!-- /.navbar-collapse -->

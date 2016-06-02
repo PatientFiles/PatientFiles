d518fe8b753b2e2cb95a8a840b42dee73b1f83c3
Ada Lacanlale <lacanlaleadada@gmail.com>
2016-06-02 17:10:23 +0800

    Consultation page: Adding of vitals function

--- john/resources/views/pages/consultation.blade.php
+++ john/resources/views/pages/consultation.blade.php
@@ -22,7 +22,6 @@
           </small>
              
       @endif
-
 <div class="row">
   <div class="col-lg-3 col-sm-12">
    <div class="box box-primary">
@@ -92,17 +91,14 @@
               <div class="row form-group">
                   <div class="col-lg-6">
                       <label for="number">Height:</label>
-                      @if($errors->has('height'))
-                          <span class="error" style="color: red">{{ $errors->first('height') }}</span>
-                      @endif
-                      <input id="height" type="number" name="height" class="form-control" placeholder="Height in centimeters">
+                      <input max="999" type="number" name="height" class="form-control" placeholder="Height in centimeters">
                   </div>
                   <div class="col-lg-6">
                       <label for="number">Weight:</label>
                       @if($errors->has('weight'))
                           <span class="error" style="color: red">{{ $errors->first('weight') }}</span>
                       @endif
-                      <input id="weight" type="number" name="weight" class="form-control" placeholder="Weight in kilograms">
+                      <input max="999" type="number" name="weight" class="form-control" placeholder="Weight in kilograms">
                   </div>
               </div>
 
@@ -112,14 +108,14 @@
                       @if($errors->has('pulse'))
                           <span class="error" style="color: red">{{ $errors->first('pulse') }}</span>
                       @endif
-                    <input id="pulse" type="number" name="pulse" class="form-control" placeholder="Pulse Rate (Pulse per minute)">
+                    <input max="999" type="number" name="pulse" class="form-control" placeholder="Pulse Rate (Pulse per minute)">
                   </div>
                   <div class="col-lg-6">
                     <label for="number">Respiratory Rate:</label>
                       @if($errors->has('respiratory'))
                           <span class="error" style="color: red">{{ $errors->first('respiratory') }}</span>
                       @endif
-                    <input id="respiratory" type="number" name="respiratory" class="form-control" placeholder="Respiratory Rate">
+                    <input max="999" type="number" name="respiratory" class="form-control" placeholder="Respiratory Rate">
                   </div>
               </div>
               
@@ -129,7 +125,7 @@
                       @if($errors->has('temp'))
                           <span class="error" style="color: red">{{ $errors->first('temp') }}</span>
                       @endif
-                    <input id="temp" type="number" name="temp" class="form-control" placeholder="Temperature in Celsius">
+                    <input max="999" type="number" name="temp" class="form-control" placeholder="Temperature in Celsius">
                   </div>
                   <div class="col-lg-6">
                     <label class="control-label" for="mens">Last Menstrual:</label>
@@ -137,7 +133,7 @@
                           <span class="error" style="color: red">{{ $errors->first('mens') }}</span>
                       @endif
                       <div class="input-group">
-                          <input id="mens" style="z-index: 100000;" name="mens" type="text" name="idTourDateDetails" id="idTourDateDetails" readonly="readonly" class="form-control" placeholder="Click here to pick date of last menstruation">
+                          <input name="mens" type="text" name="idTourDateDetails" id="idTourDateDetails" readonly="readonly" class="form-control" placeholder="Click here to pick date of last menstruation">
                           <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
                       </div>
                   </div>
@@ -149,14 +145,14 @@
                       @if($errors->has('sys'))
                           <span class="error" style="color: red">{{ $errors->first('sys') }}</span>
                       @endif
-                    <input id="sys" type="number" name="sys" class="form-control" placeholder="Systolic Value">
+                    <input max="999" type="number" name="sys" class="form-control" placeholder="Systolic Value">
                   </div>
                   <div class="col-lg-6">
                     <label for="number">Blood Pressure (Diastolic):</label>
                       @if($errors->has('dia'))
                           <span class="error" style="color: red">{{ $errors->first('dia') }}</span>
                       @endif
-                    <input id="dia" type="number" name="dia" class="form-control" placeholder="Diastolic Value">
+                    <input max="999" type="number" name="dia" class="form-control" placeholder="Diastolic Value">
                   </div>
               </div>
 
@@ -166,12 +162,13 @@
                       @if($errors->has('notes'))
                           <span class="error" style="color: red">{{ $errors->first('notes') }}</span>
                       @endif
-                <textarea id="notes" name="notes" class="form-control" rows="5" placeholder="Doctors vital notes"></textarea>
+                <textarea name="notes" class="form-control" rows="5" placeholder="Doctors vital notes"></textarea>
               </div>
 
           </div>
           <div class="modal-footer">
-            <button idtype="submit" name="saveVitals" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+            <button type="submit" name="saveVitals" class="btn btn-primary">Save</button>
           </div>
          </form>
       </div>

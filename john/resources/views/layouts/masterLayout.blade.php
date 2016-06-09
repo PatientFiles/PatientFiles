1e9f5a4d9a54b872438fdd2becd509d38bccd5e7
jb <decastro.johnbendict@gmail.com>
2016-06-09 14:40:25 +0800

    Items : Add Forms

--- john/resources/views/layouts/masterLayout.blade.php
+++ john/resources/views/layouts/masterLayout.blade.php
@@ -410,13 +410,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
  <!-- CHART DONUT -->
 
-
-
-
-
-
-
-
  <script >
    $("#scrollButton").click(function () {
     scrollCalendarToCurrentTime();

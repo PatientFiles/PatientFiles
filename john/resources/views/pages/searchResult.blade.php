5103661ef4e2ee7e665d6f5885e6e5f15e8864a3
marvs000 <marvinbaste000@gmail.com>
2016-06-24 18:05:21 +0500

    Bug fixes on searching

--- john/resources/views/pages/searchResult.blade.php
+++ john/resources/views/pages/searchResult.blade.php
@@ -75,6 +75,69 @@
                     </div> <!-- END ROW -->
                  </li>  <!-- END LI -->
                </ul> <!-- END UL -->   
-           </div>  <!-- END WHOLE ROW -->   
+           </div>  <!-- END WHOLE ROW --> 
+           <div id="newVisit-{{$patient->id}}" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <form action="/new_appointment" method="POST">
+      <div class="modal-content" >
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" style="color: white">Patient Visit</h4>
+        </div>
+        <div class="modal-body">
+            <strong>Chief Complaints<span style="color: red">*</span></strong>
+            @if($errors->has('chief_complaints'))
+                    <span class="error" style="color: red">{{ $errors->first('chief_complaints') }}</span>
+                @endif
+                {!! csrf_field() !!}
+            <input type="text" name="chief_complaints" class="form-control" placeholder="Chief Complaint" aria-describedby="basic-addon1" required>
+            <input type="hidden" name="patient_id" class="form-control" placeholder="Chief Complaint" aria-describedby="basic-addon1" value="{{ $patient->id }}">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+          <input type="submit" class="btn btn-primary" value="Start Visit" />
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+<div id="patientVisit-{{$patient->id}}" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <form action="/old_appointment" method="POST" role="form">
+    <div class="modal-content" >
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title" style="color: white">Patient Visit</h4>
+      </div>
+      <div class="modal-body">
+              <strong>Purpose<span style="color: red">*</span></strong>
+              @if($errors->has('purpose_id'))
+                      <span class="error" style="color: red">{{ $errors->first('purpose_id') }}</span>
+                  @endif
+                {!! csrf_field() !!}
+              <select class="form-control" name="purpose_id" style="height: 34px; width:100%" required>
+                    <option value disabled selected>Select...</option>
+                    <option value="2">Old Patient Visit</option>
+                    <option value="3">Follow-up Visit</option>
+             </select>
+      </div>
+      <div class="modal-body">
+                  <strong>Chief Complaints<span style="color: red">*</span></strong>
+                  @if($errors->has('chief_complaints'))
+                          <span class="error" style="color: red">{{ $errors->first('chief_complaints') }}</span>
+                      @endif
+                    <input type="text" name="chief_complaints" class="form-control" placeholder="Chief Complaint" aria-describedby="basic-addon1" required>                  
+                    <input type="hidden" name="patient_id" class="form-control" placeholder="Chief Complaint" aria-describedby="basic-addon1" value="{{ $patient->id }}">
+    </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <input type="submit" class="btn btn-primary" value="Start Visit" />
+      </div>
+    </div>
+</form>
+  </div>
+</div>  
            @endforeach
 @stop
